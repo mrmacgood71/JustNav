6028c293fcea2416d87c6f842fd1585244d1fca9
macgood <bo2002277@gmail.com>
2023-03-30 00:28:55 +0300

    Initial commit

---
+++ JustNav/src/main/java/it/macgood/justnav/JustNavigation.kt
@@ -0,0 +1,40 @@
+package it.macgood.justnav
+
+import android.app.Activity
+import android.view.ViewParent
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+
+fun Fragment.navigate(action: Int) {
+    try {
+        val activity = requireActivity() as Activity
+        var parent = requireView().parent
+        parent = findContainer(parent)
+        val navController = activity.findNavController(parent.id)
+        navController.navigate(action)
+    } catch (e: Exception) {
+        error("Navigation Error")
+    }
+}
+
+fun RecyclerView.ViewHolder.navigate(action: Int) {
+    try {
+        val activity = itemView.context as Activity
+        var parent = itemView.parent
+        parent = findContainer(parent)
+        val navController = activity.findNavController(parent.id)
+        navController.navigate(action)
+    } catch (e: java.lang.Exception) {
+        error("Navigation Error")
+    }
+}
+
+private fun findContainer(view: ViewParent): FragmentContainerView {
+    var parent = view
+    while (parent !is FragmentContainerView) {
+        parent = parent.parent
+    }
+    return parent
+}
